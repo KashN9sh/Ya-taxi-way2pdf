52a6e749ec135960de6a0904438c5330bb380add
KashN9sh <kash244@yandex.ru>
2021-06-21 22:02:59 +0300

    Base of data
    Added creating pdf from json base

--- product_scraper/product_scraper/spiders/ecom_spider.py
+++ product_scraper/product_scraper/spiders/ecom_spider.py
@@ -9,6 +9,8 @@ from reportlab.pdfgen.canvas import Canvas
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 import sys
+import os
+import json
 from PyQt5 import QtWidgets
 import MainWindow 
 
@@ -88,31 +90,32 @@ def parse(gosReg):
             time.sleep(1)
 
             j += 1
+
         '''
         f.write(item.gosReg + ' : {\n')
 
-        f.write('status : ' + item.status + '\n')
-        f.write('date : ' + item.date + '\n')
-        f.write('regNum : ' + item.regNum + '\n')
-        f.write('carrier : ' + item.carrier + '\n')
-        f.write('ogrn : ' + item.ogrn + '\n') 
-        f.write('inn : ' + item.inn + '\n')
-        f.write('markAuto : ' + item.markAuto + '\n')
-        f.write('modelAuto : ' + item.modelAuto + '\n')
-        f.write('gosReg : ' + item.gosReg + '\n')
-        f.write('yearOfCreate : ' +  item.yearOfCreate + '\n')
-        f.write('NumberOfResolution : '  + item.NumberOfResolution + '\n')
-        f.write('CompeteUL : ' + item.CompeteUL + '\n')
-        f.write('DateOfCompete : ' + item.DateOfCompete + '\n')
-        f.write('Region : ' + item.Region + '\n')
+        f.write('"status" : ' + '"' + item.status + '"' + ', \n')
+        f.write('"date" : ' + '"' + item.date + '"' + ', \n')
+        f.write('"regNum" : ' + '"' + item.regNum + '"' + ', \n')
+        f.write('"carrier" : ' + '"' + item.carrier + '"' + ', \n')
+        f.write('"ogrn" : ' + '"' + item.ogrn + '"' + ', \n') 
+        f.write('"inn" : ' + '"' + item.inn + '"' + ', \n')
+        f.write('"markAuto" : ' + '"' + item.markAuto + '"' + ', \n')
+        f.write('"modelAuto" : ' + '"' + item.modelAuto + '"' + ', \n')
+        f.write('"gosReg" : ' + '"' + item.gosReg + '"' + ', \n')
+        f.write('"yearOfCreate" : ' + '"' +  item.yearOfCreate + '"' + ', \n')
+        f.write('"NumberOfResolution" : '  + '"' + item.NumberOfResolution + '"' + ', \n')
+        f.write('"CompeteUL" : ' + '"' + item.CompeteUL + '"' + ', \n')
+        f.write('"DateOfCompete" : ' + '"' + item.DateOfCompete + '"' + ', \n')
+        f.write('"Region" : ' + '"' + item.Region + '"' + ', \n')
         '''
 
-        time.sleep(1)
+        time.sleep(3)
 
         next = driver.find_element_by_xpath("//img[@alt='QR']")
         next.click()
 
-        time.sleep(1)
+        time.sleep(3)
 
         inn = {
             'inn' : item.inn
@@ -120,13 +123,13 @@ def parse(gosReg):
         b_kontur_url = f'https://www.b-kontur.ru/profi/okpo-po-inn-ili-ogrn?{urllib.parse.urlencode(inn)}'
         
         driver.get(b_kontur_url)
-        time.sleep(2)
+        time.sleep(3)
 
         OKPO = driver.find_elements_by_xpath("//dd")[2].text
         item.OKPO = OKPO
 
-        '''    
-        f.write('OKPO : ' + item.OKPO + '\n')
+        '''
+        f.write('"OKPO" : ' + '"' + item.OKPO + '"' + ', \n')
         f.write('}\n')
 
         f.close()
@@ -138,10 +141,12 @@ def parse(gosReg):
 
 def makePDF(Carrier, dateFrom, dateTo):
     date = dateFrom
-    canvas = Canvas("font-colors.pdf", pagesize = landscape(A4))
+    canvas = Canvas(Carrier.name.split()[0] + ".pdf", pagesize = landscape(A4))
 
     pdfmetrics.registerFont(TTFont('FreeSans', 'FreeSans.ttf'))
     pdfmetrics.registerFont(TTFont('FreeSansBold', 'FreeSansBold.ttf'))
+
+    i = 1
 
     while date != dateTo + timedelta(days = 1):
         canvas.setFont('FreeSans', 8)
@@ -199,37 +204,36 @@ def makePDF(Carrier, dateFrom, dateTo):
         canvas.drawString(90 * mm, 138 * mm, Carrier.category)
 
         canvas.drawString(75 * mm, 142 * mm, 'Гаражный номер: ')
-        canvas.drawString(100 * mm, 142 * mm, Carrier.garajeNumber)
+        canvas.drawString(99 * mm, 142 * mm, Carrier.garajeNumber)
 
         canvas.drawString(75 * mm, 148 * mm, 'Регистрационный № ')
-        canvas.drawString(106 * mm, 148 * mm, Carrier.regNum + ' ' + Carrier.NumberOfResolution)
+        canvas.drawString(103 * mm, 148 * mm, Carrier.regNum + ' ' + Carrier.NumberOfResolution)
 
         canvas.drawString(10 * mm, 138 * mm, 'Водительское удостоверение №')
-        canvas.drawString(54 * mm, 138 * mm, Carrier.driverLicense)
+        canvas.drawString(53 * mm, 138 * mm, Carrier.driverLicense)
 
-        canvas.drawString(10 * mm, 142 * mm, 'Водитель')
-        canvas.drawString(30 * mm, 142 * mm, Carrier.name)
+        canvas.drawString(10 * mm, 142 * mm, 'Водитель:')
+        canvas.drawString(24 * mm, 142 * mm, Carrier.name.split()[0] + '.' + Carrier.name.split()[1][0] + '.' + Carrier.name.split()[2][0])
 
-        canvas.drawString(10 * mm, 148 * mm, 'Лицензионная карточка: ')
-        canvas.line(44 * mm, 147.5 * mm, 74 * mm, 147.5 * mm)
+        canvas.drawString(10 * mm, 148 * mm, 'Лицензионная карточка: стандартная')
 
         canvas.drawString(10 * mm, 152 * mm, 'Государственный номерной знак: ')
-        canvas.drawString(60 * mm, 152 * mm, Carrier.gosReg)
+        canvas.drawString(55 * mm, 152 * mm, Carrier.gosReg)
 
         canvas.drawString(10 * mm, 156 * mm, 'Марка и модель автомобиля: ')
-        canvas.drawString(55 * mm, 156 * mm, Carrier.markAuto + ' ' + Carrier.modelAuto)
+        canvas.drawString(49 * mm, 156 * mm, Carrier.markAuto + ' ' + Carrier.modelAuto)
 
         canvas.drawString(10 * mm, 159 * mm, 'Организация: ')
-        canvas.drawString(30 * mm, 159 * mm, Carrier.CompeteUL )
+        canvas.drawString(29 * mm, 159 * mm, Carrier.CompeteUL )
 
         canvas.drawString(10 * mm, 192 * mm, 'Телефон: ')
-        canvas.drawString(30 * mm, 192 * mm, Carrier.phoneNumber)
+        canvas.drawString(25 * mm, 192 * mm, Carrier.phoneNumber)
 
         canvas.drawString(10 * mm, 196 * mm, 'Адрес: ')
-        canvas.drawString(30 * mm, 196 * mm, Carrier.addres)
+        canvas.drawString(20 * mm, 196 * mm, Carrier.addres)
 
         canvas.drawString(10 * mm, 200 * mm, 'Перевозчик: ')
-        canvas.drawString(30 * mm, 200 * mm, Carrier.carrier)
+        canvas.drawString(27 * mm, 200 * mm, Carrier.carrier)
 
         canvas.drawString(105 * mm, 192 * mm, 'ОГРН: ')
         canvas.drawString(116 * mm, 192 * mm, Carrier.ogrn)
@@ -238,9 +242,9 @@ def makePDF(Carrier, dateFrom, dateTo):
         canvas.drawString(120 * mm, 196 * mm, Carrier.inn)
 
         canvas.drawString(113 * mm, 200 * mm, 'ОКПО: ')
-        canvas.drawString(125 * mm, 200 * mm, Carrier.OKPO)
+        canvas.drawString(124 * mm, 200 * mm, Carrier.OKPO)
 
-        canvas.drawString(60 * mm, 175 * mm, date.strftime('«‎%d»‎ %B %Y'))
+        canvas.drawString(63 * mm, 175 * mm, date.strftime('«‎%d»‎ %B %Y'))
 
         canvas.setFont('FreeSans', 6)
 
@@ -291,9 +295,11 @@ def makePDF(Carrier, dateFrom, dateTo):
 
         canvas.setFont('FreeSansBold', 6)
         canvas.drawString(64 * mm, 182 * mm, 'серия:')
+        canvas.drawString(72 * mm, 182 * mm, Carrier.CompeteUL.split()[2][0] + Carrier.CompeteUL.split()[3][0] + ' № ' + str(i))
 
         canvas.showPage()
         date += timedelta(days = 1)
+        i += 1
 
     canvas.save()
 
@@ -305,22 +311,94 @@ if __name__ == "__main__":
     # args[2:] = function args : (*unpacked)
     globals()[args[1]](*args[2:])
 '''
+
+def makeCarrierFromJSON(data):
+    man = Carrier()
+    man.status = data['status']
+    man.date = data['date']
+    man.regNum = data['regNum']
+    man.carrier = data['carrier']
+    man.ogrn = data['ogrn']
+    man.inn = data['inn']
+    man.markAuto = data['markAuto']
+    man.modelAuto = data['modelAuto']
+    man.gosReg = data['gosReg']
+    man.yearOfCreate  = data['yearOfCreate']
+    man.NumberOfResolution = data['NumberOfResolution']
+    man.CompeteUL = data['CompeteUL']
+    man.DateOfCompete  = data['DateOfCompete']
+    man.Region = data['Region']
+    man.OKPO = data['OKPO']
+    man.name = data['name']
+    man.addres = data['addres']
+    man.phoneNumber = data['phoneNumber']
+    man.garajeNumber = data['garajeNumber']
+    man.driverLicense = data['driverLicense']
+    man.category = data['category']
+
+    return man
+
+
 class App(QtWidgets.QMainWindow, MainWindow.Ui_Dialog):
+    bd = []
+    loadFromBD = False
+    carrierFromBDNum = 0
+
     def __init__(self):
         super().__init__()
         self.setupUi(self) 
         self.workbtn.clicked.connect(self.work)
-    
-    def work(self):
-        CarrierForParse = Carrier()
-        CarrierForParse = parse(self.gosNum.text())
-        CarrierForParse.name = self.name.text()
-        CarrierForParse.addres = self.addres.text()
-        CarrierForParse.phoneNumber = self.phoneNumber.text()
-        CarrierForParse.garajeNumber = self.garajeNumber.text()
-        CarrierForParse.driverLicense = self.driverLicense.text()
-        CarrierForParse.category = self.category.text()
-        makePDF(CarrierForParse,self.dateEditFrom.date().toPyDate(), self.dateEditTo.date().toPyDate())
+        self.gosNum.textChanged.connect(self.autoFill)
+        
+        f = open("demofile2.txt", "r")
+        for line in f:
+            self.bd.append(makeCarrierFromJSON(json.loads(line)))   
+
+        f.close()
+
+    def autoFill(self):
+        i = 0
+        for carrier in self.bd:
+            if self.gosNum.text().lower() == carrier.gosReg.lower() :
+                self.gosNum.setText(carrier.gosReg)
+                self.name.setText(carrier.name)
+                self.addres.setText(carrier.addres)
+                self.phoneNumber.setText(carrier.phoneNumber)
+                self.garajeNumber.setText(carrier.garajeNumber)
+                self.driverLicense.setText(carrier.driverLicense)
+                self.category.setText(carrier.category)
+                self.loadFromBD = True
+                self.carrierFromBDNum = i
+            else:
+                self.loadFromBD = False
+
+            i += 1
+
+    def work(self):  
+        if not self.loadFromBD:
+            CarrierForParse = Carrier()
+            CarrierForParse = parse(self.gosNum.text())
+            CarrierForParse.name = self.name.text()
+            CarrierForParse.addres = self.addres.text()
+            CarrierForParse.phoneNumber = self.phoneNumber.text()
+            CarrierForParse.garajeNumber = self.garajeNumber.text()
+            CarrierForParse.driverLicense = self.driverLicense.text()
+            CarrierForParse.category = self.category.text()
+            jsonStr = json.dumps(CarrierForParse.__dict__,ensure_ascii=False)
+
+            f = open("demofile2.txt", "a")
+            f.write(jsonStr)
+            f.close()
+
+            makePDF(CarrierForParse,self.dateEditFrom.date().toPyDate(), self.dateEditTo.date().toPyDate())
+        else:
+            self.bd[self.carrierFromBDNum].name = self.name.text()
+            self.bd[self.carrierFromBDNum].addres = self.addres.text()
+            self.bd[self.carrierFromBDNum].phoneNumber = self.phoneNumber.text()
+            self.bd[self.carrierFromBDNum].garajeNumber = self.garajeNumber.text()
+            self.bd[self.carrierFromBDNum].driverLicense = self.driverLicense.text()
+            self.bd[self.carrierFromBDNum].category = self.category.text()
+            makePDF(self.bd[self.carrierFromBDNum],self.dateEditFrom.date().toPyDate(), self.dateEditTo.date().toPyDate())
 
 def main():
     app = QtWidgets.QApplication(sys.argv) 
