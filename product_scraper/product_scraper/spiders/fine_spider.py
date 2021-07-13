de43ef48032228085f625c4cba046d3d74e936cd
KashN9sh <kash244@yandex.ru>
2021-07-13 13:11:28 +0300

    Fines array

--- product_scraper/product_scraper/spiders/fine_spider.py
+++ product_scraper/product_scraper/spiders/fine_spider.py
@@ -1,15 +1,16 @@
 import time
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from dateutil import parser
 
+class Fine:
+    date: datetime.date
+    time:datetime.time
+    decree: int
+    cost: int
 
 def parse_info(gos_reg, region, registration):
-    params = {
-        'number': gos_reg,
-        'region': region,
-        'registration': registration
-    }
-
+    fines_array = []
     fines_url = f'https://гибдд.рф/check/fines' \
                 f'#{gos_reg}+{region}+{registration}'
 
@@ -37,20 +38,24 @@ def parse_info(gos_reg, region, registration):
     fines_count = len(driver.find_elements_by_xpath("//div[@class='checkResult']/ul[@class='finesItem']"))
 
     for i in range(fines_count - 1):
-        date = driver.find_elements_by_xpath("//div[@class='checkResult']/"
+        current_fine =  Fine
+        date_str = driver.find_elements_by_xpath("//div[@class='checkResult']/"
                                              "ul[@class='finesItem']/li/span[@class='field fine-datedecis']")[i].text
-        decree = driver.find_elements_by_xpath("//div[@class='checkResult']/"
+        decree_str = driver.find_elements_by_xpath("//div[@class='checkResult']/"
                                              "ul[@class='finesItem']/li/span[@class='field fine-datepost']")[i].text
-        cost = driver.find_elements_by_xpath("//div[@class='checkResult']/"
+        cost_str = driver.find_elements_by_xpath("//div[@class='checkResult']/"
                                                "ul[@class='finesItem']/li/span[@class='field fine-summa']")[i].text
 
-        print(date)
-        print(decree)
-        print(cost)
+        current_fine.date = parser.parse(date_str.split()[0])
+        current_fine.time = parser.parse(date_str.split()[2])
+        current_fine.decree = decree_str.split()[0]
+        current_fine.cost = cost_str.split()[0]
+
+        fines_array.append(current_fine)
 
     driver.close()
 
-    # return item
+     return fines_array
 
 
 parse_info('У468ВХ', '797', '9931918970')
